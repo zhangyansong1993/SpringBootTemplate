fa5e8f7d2486793bdb6df0d804ec8fadc4ee0ffc
zhangyansong <zhangyansong@act-telecom.com>
2022-01-05 10:58:57 +0800

    es基本查询api

--- src/main/java/com/zys/utils/ESUtils.java
+++ src/main/java/com/zys/utils/ESUtils.java
@@ -1,12 +1,15 @@
 package com.zys.utils;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
@@ -18,8 +21,14 @@ import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.index.reindex.UpdateByQueryRequest;
 import org.elasticsearch.script.Script;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.*;
+import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.sort.ScriptSortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
@@ -28,10 +37,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class ESUtils {
@@ -83,6 +89,7 @@ public class ESUtils {
                 DeleteIndexRequest request = new DeleteIndexRequest(indexName);
                 AcknowledgedResponse response = client.indices().delete(request, options);
                 return response.isAcknowledged();
+                //return client.indices().delete(new DeleteIndexRequest(indexName), options).isAcknowledged();
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -90,15 +97,16 @@ public class ESUtils {
         return Boolean.FALSE;
     }
 
+
     /**
      * 写入数据
      */
-    public boolean insert(String indexName, Map<String, Object> dataMap) {
+    public boolean insert(String indexName, JSONObject obj) {
         try {
-            BulkRequest request = new BulkRequest();
-            request.add(new IndexRequest(indexName).id(dataMap.remove("id").toString())
-                    .opType("create").source(dataMap, XContentType.JSON));
-            this.client.bulk(request, options);
+            IndexRequest indexRequest = new IndexRequest(indexName);
+            indexRequest.source(obj, XContentType.JSON);
+            IndexResponse index = this.client.index(indexRequest, options);
+            System.out.println(index.getResult());
             return Boolean.TRUE;
         } catch (Exception e) {
             e.printStackTrace();
@@ -109,14 +117,16 @@ public class ESUtils {
     /**
      * 批量写入数据
      */
-    public boolean batchInsert(String indexName, List<Map<String, Object>> userIndexList) {
+    public boolean insertBatch(String indexName, JSONArray obj) {
         try {
-            BulkRequest request = new BulkRequest();
-            for (Map<String, Object> dataMap : userIndexList) {
-                request.add(new IndexRequest(indexName).id(dataMap.remove("id").toString())
-                        .opType("create").source(dataMap, XContentType.JSON));
+            BulkRequest request = new BulkRequest(indexName);
+            for (Object o : obj) {
+                IndexRequest indexRequest = new IndexRequest();
+                indexRequest.source(o.toString(), XContentType.JSON);
+                request.add(indexRequest);
             }
-            this.client.bulk(request, options);
+            BulkResponse bulk = this.client.bulk(request, options);
+            System.out.println(bulk.getTook());
             return Boolean.TRUE;
         } catch (Exception e) {
             e.printStackTrace();
@@ -127,12 +137,34 @@ public class ESUtils {
     /**
      * 更新数据，可以直接修改索引结构
      */
-    public boolean update(String indexName, Map<String, Object> dataMap) {
+    public boolean update(String indexName, String docId, JSONObject obj) {
         try {
-            UpdateRequest updateRequest = new UpdateRequest(indexName, dataMap.remove("id").toString());
-            updateRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE);
-            updateRequest.doc(dataMap);
+            UpdateRequest updateRequest = new UpdateRequest(indexName, docId);
+            //UpdateRequest updateRequest = new UpdateRequest();
+            //or updateRequest.index("user").id("0001");
+            updateRequest.doc(obj);
             this.client.update(updateRequest, options);
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Boolean.FALSE;
+    }
+
+    /**
+     * 条件更新
+     */
+    public boolean updateWhere(String indexName) {
+        try {
+            UpdateByQueryRequest updateByQueryRequest = new UpdateByQueryRequest(indexName);
+//            updateByQueryRequest.indices("user");
+            BoolQueryBuilder builder = QueryBuilders.boolQuery();
+            builder.must(QueryBuilders.matchPhraseQuery("name", "张三"));
+            updateByQueryRequest.setQuery(builder);
+            updateByQueryRequest.setScript(new Script("ctx._source['tel']=16666666666L")); //字符串单引号，数值加L
+            //数据为存储而不是更新
+            BulkByScrollResponse response = this.client.updateByQuery(updateByQueryRequest, RequestOptions.DEFAULT);
+            System.out.println(response.getStatus().getUpdated());//更新数量
             return Boolean.TRUE;
         } catch (Exception e) {
             e.printStackTrace();
@@ -153,6 +185,72 @@ public class ESUtils {
         }
         return Boolean.FALSE;
     }
+
+    /**
+     * 批量删除数据
+     */
+    public boolean deleteBatch(String indexName, String[] array) {
+        try {
+            BulkRequest request = new BulkRequest(indexName);
+            for (String str : array) {
+                DeleteRequest deleteRequest = new DeleteRequest();
+                deleteRequest.id(str);
+                request.add(deleteRequest);
+            }
+            BulkResponse bulk = this.client.bulk(request, options);
+            System.out.println(bulk.getTook());
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Boolean.FALSE;
+    }
+
+    /**
+     * 条件删除数据
+     */
+    public boolean deleteWhere(String indexName) {
+        try {
+            DeleteByQueryRequest delete = new DeleteByQueryRequest(indexName);
+            BoolQueryBuilder builder = QueryBuilders.boolQuery();
+            builder.must(QueryBuilders.matchPhraseQuery("name", "张三"));
+            delete.setQuery(builder);
+            BulkByScrollResponse response = this.client.deleteByQuery(delete, options);
+            System.out.println(response.getStatus().getDeleted());
+            return Boolean.TRUE;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Boolean.FALSE;
+    }
+
+
+    /**
+     * 查询
+     */
+    public List<Map> queryIndex(String indexName) {
+        List<Map> list = new ArrayList();
+        try {
+            SearchRequest searchRequest = new SearchRequest(indexName);
+            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+            BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();//构建搜索条件
+            queryBuilder.must(QueryBuilders.matchAllQuery());//match模糊匹配,match_phrase完全匹配,must--and,should--or,mustNot--!=
+
+            sourceBuilder.query(queryBuilder);
+            searchRequest.source(sourceBuilder);
+
+            SearchResponse search = client.search(searchRequest, options);
+            SearchHit[] hits = search.getHits().getHits();
+            for (SearchHit hit : hits) {
+                list.add(hit.getSourceAsMap());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
 
     /**
      * 查询总数
@@ -177,16 +275,47 @@ public class ESUtils {
     }
 
     /**
-     * 查询集合
+     * 查询总数
+     */
+    public Long count2(String indexName) {
+        Long count = 0L;
+        try {
+            // 指定创建时间
+            SearchRequest request = new SearchRequest(indexName);
+            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+            BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+            queryBuilder.must(QueryBuilders.termQuery("createTime", 1611378102795L));
+
+            sourceBuilder.query(queryBuilder);
+            request.source(sourceBuilder);
+
+            SearchResponse search = client.search(request, options);
+            count = search.getHits().getTotalHits().value;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    /**
+     * 条件查询
      */
     public List<Map<String, Object>> list(String indexName) {
-        // 查询条件,指定时间并过滤指定字段值
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
-        queryBuilder.must(QueryBuilders.termQuery("createTime", 1611378102795L));
-        queryBuilder.mustNot(QueryBuilders.termQuery("name", "北京-李四"));
-        sourceBuilder.query(queryBuilder);
+        // 查询条件,指定时间并过滤指定字段值 termQuery分词精确查询 matchPhraseQuery完全匹配不分词
         SearchRequest searchRequest = new SearchRequest(indexName);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        queryBuilder.must(QueryBuilders.matchAllQuery());
+        //matchQuery分词查询，matchPhraseQuery精确查询不分词，keyword类型字段不会被分词，text类型字段会被分词,字段index设为false不能被索引
+        queryBuilder.must(QueryBuilders.matchPhraseQuery("name", "张三"));
+//        queryBuilder.mustNot(QueryBuilders.matchPhraseQuery("name", "王五"));//mustNot需要搭配matchQuery
+//        queryBuilder.filter(QueryBuilders.rangeQuery("age").gt(30));//范围查询  gt大于gte大于等于lt小于lte小于等于
+//        queryBuilder.filter(QueryBuilders.rangeQuery("create").gte(new Date().getTime()));//范围查询
+
+        sourceBuilder.query(queryBuilder);
         searchRequest.source(sourceBuilder);
         try {
             SearchResponse searchResp = client.search(searchRequest, options);
@@ -204,6 +333,7 @@ public class ESUtils {
         return null;
     }
 
+
     /**
      * 分页查询
      */
@@ -212,17 +342,72 @@ public class ESUtils {
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
         sourceBuilder.from(offset);
         sourceBuilder.size(size);
-        sourceBuilder.sort("createTime", SortOrder.DESC);
+        sourceBuilder.sort("createTime", SortOrder.DESC);//排序
         SearchRequest searchRequest = new SearchRequest(indexName);
         searchRequest.source(sourceBuilder);
         try {
             SearchResponse searchResp = client.search(searchRequest, options);
             List<Map<String, Object>> data = new ArrayList<>();
+            long count = searchResp.getHits().getTotalHits().value;//查询命中总条数
             SearchHit[] searchHitArr = searchResp.getHits().getHits();
             for (SearchHit searchHit : searchHitArr) {
                 Map<String, Object> temp = searchHit.getSourceAsMap();
                 temp.put("id", searchHit.getId());
+                temp.put("count", count);
                 data.add(temp);
+            }
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 聚合查询
+     */
+    public List<Map<String, Object>> grouping(String indexName) {
+        // 查询条件,指定时间并过滤指定字段值
+        SearchRequest searchRequest = new SearchRequest(indexName);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        AggregationBuilder field = AggregationBuilders.terms("ageGroup").field("age");
+
+        sourceBuilder.aggregation(field);
+        searchRequest.source(sourceBuilder);
+        try {
+            SearchResponse searchResp = client.search(searchRequest, options);
+            List<Map<String, Object>> data = new ArrayList<>();
+            SearchHit[] searchHitArr = searchResp.getHits().getHits();
+            System.out.println(searchResp);
+            for (SearchHit searchHit : searchHitArr) {
+                data.add(searchHit.getSourceAsMap());
+            }
+            return data;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * max、min、avg查询
+     */
+    public List<Map<String, Object>> max(String indexName) {
+        // 查询条件,指定时间并过滤指定字段值
+        SearchRequest searchRequest = new SearchRequest(indexName);
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        AggregationBuilder field = AggregationBuilders.max("maxAge").field("age");
+
+        sourceBuilder.aggregation(field);
+        searchRequest.source(sourceBuilder);
+        try {
+            SearchResponse searchResp = client.search(searchRequest, options);
+            List<Map<String, Object>> data = new ArrayList<>();
+            SearchHit[] searchHitArr = searchResp.getHits().getHits();
+            System.out.println(searchResp);
+            for (SearchHit searchHit : searchHitArr) {
+                data.add(searchHit.getSourceAsMap());
             }
             return data;
         } catch (Exception e) {
--- src/test/java/com/zys/ApplicationTest.java
+++ src/test/java/com/zys/ApplicationTest.java
@@ -1,13 +1,14 @@
 package com.zys;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.zys.utils.ESUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.io.IOException;
 
 @RunWith(SpringRunner.class)
@@ -19,6 +20,31 @@ public class ApplicationTest {
 
     @Test
     public void test1() throws IOException {
+        System.out.println(esUtils.sort("user"));
+    }
 
+    @Test
+    public void test2() throws IOException {
+        JSONObject obj = new JSONObject();
+        obj.put("name", "张三姐");
+        obj.put("sex", "女");
+        obj.put("age", 30);
+        System.out.println(esUtils.update("user", "Abu9Cn4BtxoJVOTRqHQd", obj));
+    }
+
+    @Test
+    public void test3() throws IOException {
+        JSONArray array = new JSONArray();
+        JSONObject obj = new JSONObject();
+        obj.put("name", "张三");
+        obj.put("sex", "女");
+        obj.put("age", 30);
+        JSONObject obj1 = new JSONObject();
+        obj1.put("name", "张三");
+        obj1.put("sex", "男");
+        obj1.put("age", 300);
+        array.add(obj);
+        array.add(obj1);
+        System.out.println(esUtils.list("user"));
     }
 }
